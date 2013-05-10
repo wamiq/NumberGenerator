a9fe5120934abac2b57d6aedf75468144feb8507
wamiq <wamiq.bashir@gmail.com>
2013-05-10 08:27:59 +0100

    New Class and Tests Added annotations for JUnit to discover tests

---
+++ src/test/java/com/valtech/NumberGeneratorTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.valtech;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import com.valtech.interfaces.INumberGenerator;
+
+/**
+ * @author wamiq
+ * 
+ */
+public class NumberGeneratorTest{
+	/*
+	 *
+	 */
+	@Test
+	public void TestGenerateRandomNumber() {
+		INumberGenerator numberGenerator = new NumberGenerator();
+		assertTrue(numberGenerator.getRandomNumber() < 100);
+	}
+
+}
